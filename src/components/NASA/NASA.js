9cdc7cedfe32aa1de23534f82594fd1713eac8e9
KirstenVastine <KirstenVastine@gmail.com>
2020-10-28 22:13:01 -0400

    card updates

--- src/components/NASA/NASA.js
+++ src/components/NASA/NASA.js
@@ -3,7 +3,9 @@ import React from 'react';
 const NASA= () =>{
 
     return(
-
+        <div>
+            
+        </div>
     )
 }
 
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button} from 'reactstrap';
 
 const WeatherComponent = () => {
 
@@ -6,6 +7,18 @@ const WeatherComponent = () => {
 
         <div className="main">
             <div className="mainDiv">
+
+            <div>
+      <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
 
             </div>
 
