0f53318e9278c3e202893188fdccd092b78f0678
Jessica Bushong <jessica.bushong08@gmail.com>
2020-10-28 23:44:23 -0400

    basic template

--- src/components/NASA/NASA.js
+++ src/components/NASA/NASA.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button} from 'reactstrap';
 
 const NASA= () =>{
-
-
-=======
-    return(    <div>
+    return(   
+         <div>
         <Card>
           <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
           <CardBody>
