2cce1aa84c72e0276e5ef097964619ece853acd5
Jessica Bushong <jessica.bushong08@gmail.com>
2020-10-29 23:45:33 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,28 @@ import Sitebar from './components/Sitebar/Sitebar'
 
 function App() {
   const [pos, setPos] = useState({lat: "", long: ""})
+  const[button, setButton]= useState(false);
+
+  
+
+
+ const showCards=() =>{
+   return (button === true ? 
+  <div className="cards"><h1>Indianapolis, Indiana</h1>
+  <Row className="cardPlacement" >
+  <Col sm="3">  <NASA coord={pos} lat={pos.lat} long={pos.long}/> </Col>
+  <Col sm="3"> <Weather lat={pos.lat} long={pos.long} /> </Col> 
+  <Col sm="3">  <Restaurants lat={pos.lat} long={pos.long}/> </Col>
+</Row>
+  </div>
+  
+: <div className= "main" ><h1 className="title">EXPLORE YOUR </h1> <h1 className="title"> SURROUNDINGS </h1> 
+<a className="homeButton" onClick={(e)=> setButton(true)}><i class="fab fa-wpexplorer"></i>
+</a></div>
+   
+ )
+ }
+ 
   
 
     const  getLocation = () => {
@@ -32,28 +54,20 @@ function App() {
     useEffect(() => {
       getLocation();
     }, [])
+
+    
     // console.log(pos.lat)
     // console.log(pos.long)
 
   return (
-    <div>
-    <div className="cards">
-    <h1>EXPLORE YOUR SURROUNDINGS</h1>
-    <br/>
-    <br />
-    <Row className="cardPlacement">
-    <Col sm="3">  <NASA coord={pos} lat={pos.lat} long={pos.long}/> </Col>
-    <Col sm="3"> <Weather lat={pos.lat} long={pos.long} /> </Col> 
-    <Col sm="3">  <Restaurants lat={pos.lat} long={pos.long}/> </Col>
+ 
+    <div >
+ 
     
-   
-  </Row>
-  <div>
-      
-        </div>
-    </div>
+    {showCards()}
+    
+    
   
-    
     </div>
   
   );
